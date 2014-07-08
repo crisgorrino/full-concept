9a4a054dbe61af9e5d209c5233528c6ae985dd55
Cuauhtémoc <vientoquesurcalosmares@hotmail.com>
2014-07-08 11:03:03 -0500

    Se elimino archivo de migracion con nombre largo, y se modifico el header del admin para que funcione correctamente el logout

--- app/views/admin/header.blade.php
+++ app/views/admin/header.blade.php
@@ -3,7 +3,7 @@
         <ul>
             <li><a href="{{ url('admin/proyectos') }}" class="">Proyectos</a></li>
             <li><a href="{{ url('admin/comentarios') }}" class="">Comentarios</a></li>
-            <li><a href="{{ url('logout') }}">Cerrar Sesi&oacute;n</a></li>
+            <li><a href="{{ url('admin/logout') }}">Cerrar Sesi&oacute;n</a></li>
         </ul>
     </nav>	
 @stop
